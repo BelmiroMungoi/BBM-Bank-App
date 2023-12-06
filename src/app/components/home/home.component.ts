dc29487d26293dd2ede077acefe511a859d11211
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-12-06 11:00:23 +0200

    Getting the account details by authenticated user

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BankAccountService } from 'src/app/services/bank-account.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
 
+  accountNumber!: string;
+  accountBalance!: string;
+
+  constructor(private accountService: BankAccountService) {}
+
+  ngOnInit(): void {
+    this.getAccountByUser();
+  }
+
+  public getAccountByUser() {
+    this.accountService.getAccountByAuthenticatedUser().subscribe(data => {
+      this.accountNumber = data.accountNumber;
+      this.accountBalance = data.accountBalance;
+      console.log(data);
+    })
+  }
 }
