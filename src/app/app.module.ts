3a1211968c0447a4b8fbc099ad97f023e36adf3a
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-12-06 10:57:58 +0200

    Adding new modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HeaderInterceptorModule } from './services/token-interceptor.service';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,9 @@ import { RegisterComponent } from './components/auth/register/register.component
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    HttpClientModule,
+    HeaderInterceptorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
